ae4805199219b6583e7c47456a150e90b62df593
czhang <chi.zhang@jpl.nasa.gov>
2014-12-25 09:24:23 -0800

    removed platform/ from repo

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/czhang/Development/IdeaProjects/cgb/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/czhang/Development/IdeaProjects/cgb/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/czhang/Development/IdeaProjects/cgb/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/czhang/Development/IdeaProjects/cgb/platforms/android/AndroidManifest.xml \
